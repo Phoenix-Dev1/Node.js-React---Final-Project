ea6972b39f9b69c94c0f796cb39333bd7f7160bd
Phoenix-Dev2 <116957217+Phoenix-Dev2@users.noreply.github.com>
2023-06-02 19:05:23 +0300

    Repairs:Header - Main - Footer

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,12 +1,10 @@
+import React from 'react';
 import classes from './Main.module.css';
 
 function Main() {
   return (
     <main className={classes.main}>
-      <Book bookDetails={data[0]} />
-      <Book bookDetails={data[1]} />
-      <Book bookDetails={data[2]} />
-      <Book bookDetails={data[3]} />
+      <p>02/06/2023</p>
     </main>
   );
 }
