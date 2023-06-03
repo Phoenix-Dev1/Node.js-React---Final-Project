3315c53ea055502cd383e0af043fe2f98047974c
Phoenix-Dev2 <116957217+Phoenix-Dev2@users.noreply.github.com>
2023-06-03 21:26:46 +0300

    logout done

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
 import styles from './Header.module.css';
 import { NavItem, NavLink } from './NavItem/NavItem';
 import Logo from './logo';
 import EcLogo from '../../images/sm-logo.png';
 
 const Header = () => {
+  // Show user name
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Logo src={EcLogo} alt="Eco Colletors" />
+        {currentUser && (
+          <li className={styles.userName}>{currentUser.username}</li>
+        )}
+        {!currentUser && (
+          <NavItem>
+            <NavLink href="/register">Register</NavLink>
+          </NavItem>
+        )}
+        {currentUser ? (
+          <NavItem>
+            <NavLink href="/">
+              <li onClick={logout}>Logout</li>
+            </NavLink>
+          </NavItem>
+        ) : (
+          <NavItem>
+            <NavLink href="/login">Login</NavLink>
+          </NavItem>
+        )}
       </div>
       <nav className={styles.nav}>
         <ul className={styles.links}>
           <NavItem>
-            <NavLink href="/register">Register</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/login">Login</NavLink>
+            <NavLink href="/">Home</NavLink>
           </NavItem>
           <NavItem>
             <NavLink href="/releases">Link 3</NavLink>
